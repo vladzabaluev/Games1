fcdb50118936bcdf88552a9222e28afd76debf48
vladzabaluev <vladzabaluev014@gmail.com>
2021-08-31 01:28:18 +0300

    Add score and Ball Spawner
    Версия до пробы спавна шаров

--- Monkeys Volleyball/Assets/Scripts/Ball.cs
+++ Monkeys Volleyball/Assets/Scripts/Ball.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    public Rigidbody2D rb;
+   Rigidbody2D rb;
 
     public float force = 40;
     public float power = 20;
+
+    int scoreFP = 0; //first player
+    int scoreSP = 0; //second player
+
+    public Transform FPBallSpawner;
+    public Transform SPBallSpawner;
+    public GameObject theBall;
+    public GameObject createdBall;
     // Start is called before the first frame update
     void Start()
     {
-      
+        rb = theBall.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -25,11 +33,25 @@ public class Ball : MonoBehaviour
         if (collision.CompareTag("Player"))
         {       
             rb.AddForce(10 * Vector3.up * force);
+           // theBall.GetComponent<Rigidbody2D>().AddForce(10 * Vector3.up * force);
         }
         if (collision.CompareTag("Wall"))
         {
             rb.AddForce(power * Vector2.Reflect(transform.position, Vector2.right));
         }       
+        if (collision.CompareTag("FirstPlayerBot"))
+        {
+            Debug.Log(++scoreSP);
+            Destroy(theBall);
+            Instantiate(theBall, FPBallSpawner.position, Quaternion.identity);
+        }
+        else if (collision.CompareTag("SecondPlayerBot"))
+        {
+            Debug.Log(++scoreFP);
+            Destroy(theBall);
+            Instantiate(theBall, SPBallSpawner.position, Quaternion.identity);
+        }
+
     }
 
     private void OnTriggerStay2D(Collider2D collision)
